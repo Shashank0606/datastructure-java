e22645cf8210f4ea07a38d8e4c303d63c06cab0b
Shashank0606 <shashankrathore606@gmail.com>
2022-12-02 08:17:08 +0530

    REFACTOR: Adding at end Using Append

--- datastructure/src/main/java/com/bridgelabz/datastructure/Datastructure.java
+++ datastructure/src/main/java/com/bridgelabz/datastructure/Datastructure.java
@@ -1,34 +1,41 @@
 package com.bridgelabz.datastructure;
 
 public class Datastructure {
-    Node head, tail;
+	Node head, tail;
 
-    public Node push(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            Node temp = head;
-            this.head = newNode;
-            newNode.next = temp;
-        }
-        return newNode;
-    }
+	public Node push(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			Node temp = head;
+			this.head = newNode;
+			newNode.next = temp;
+		}
+		return newNode;
+	}
 
-    public void print() {
-        if (head == null) {
-            System.out.println("Linked List is Empty");
-        } else {
-            Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.key + " ");
-                temp = temp.next;
-            }
-        }
-    }
+	public void append(int data) { // 56=> 30=> 70
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			tail = newNode;
+		}
+	}
 
-    public static void main(String[] args) {
-
-    }
+	public void print() {
+		if (head == null) {
+			System.out.println("Linked List is Empty");
+		} else {
+			Node temp = head;
+			while (temp != null) {
+				System.out.print(temp.key + " ");
+				temp = temp.next;
+			}
+		}
+	}
 }
--- datastructure/src/main/java/com/bridgelabz/datastructure/Operation.java
+++ datastructure/src/main/java/com/bridgelabz/datastructure/Operation.java
@@ -4,11 +4,20 @@ public class Operation {
 
     // created method to add data at start.
     public static void addDataAtStart() {
-        LinkedList linkedList = new LinkedList();
+        Datastructure linkedList = new Datastructure();
         linkedList.push(70);
         linkedList.push(30);
         linkedList.push(56);
         linkedList.print();
     }
 
+    // Created a method to add data at end.
+    public static void addDataAtEnd() {
+        Datastructure linkedList = new Datastructure();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
+
+    }
 }
